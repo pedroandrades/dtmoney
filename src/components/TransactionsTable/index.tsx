30eefc82888baa52fe49251d159b80d34cf5fa6d
Pedro Andrades <pedro_andrades@terceiros.sicredi.com.br>
2022-03-28 20:36:49 -0300

    miragejs and axios impl

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,12 @@
-import { Container } from './styles';
+import { useEffect } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
 
 export function TransactionsTable() {
+  useEffect(() => {
+    api.get("http://localhost:3000/api/transactions").then(response => console.log(response.data));
+  });
+
   return (
     <Container>
       <table>
